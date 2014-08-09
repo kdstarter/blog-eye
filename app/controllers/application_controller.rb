20f7318fb2fa8917cfb21aeb1d69c31f6dc70fb6
agilejzl <agilejzl@gmail.com>
2014-08-10 01:59:04 +0800

    use same layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # for user devise session
+  def after_sign_in_path_for(resource_or_scope)
+    admin_root_path
+  end
+  
   def after_sign_out_path_for(resource_or_scope)
     frontend_user_path(current_user)
   end
