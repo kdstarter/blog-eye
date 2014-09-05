7ef844b5eb2cc88c20914e6bbe7df01cf46cbc89
agilejzl <agilejzl@gmail.com>
2014-09-06 00:11:32 +0800

    portal page show blogger codes

--- app/controllers/frontend/codes_controller.rb
+++ app/controllers/frontend/codes_controller.rb
@@ -1,6 +1,10 @@
 
 class Frontend::CodesController < BloggerController
   def index
-    @codes = @blogger.codes.page(params[:page_code]).per(10)
+    @codes = @blogger.codes.page(params[:page_code])
+  end
+
+  def show
+    @code = Code.find(params[:id])
   end
 end
--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -2,9 +2,8 @@
 class Frontend::HomeController < FrontendController
 
   def index
-    @users = User.limit(5).order("created_at asc")
+    @codes = Code.order("created_at desc").limit(5)
     @posts = Post.order("likes desc, created_at desc").limit(4)
-    @replies = Reply.order("created_at desc").limit(5)
   end
 
 end
