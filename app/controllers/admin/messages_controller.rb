40093c60fa9ff28f6459ef5597c3af610d65bc16
agilejzl <agilejzl@gmail.com>
2014-09-06 19:09:15 +0800

    notify blogger when post is replied

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -2,7 +2,32 @@
 class Admin::MessagesController < AdminController
 
   def index
-    @messages = @messages.page(params[:page]).per(15)
+    @messages = @messages.order('created_at desc').page(params[:page]).per(15)
   end
 
+  def show
+    @message = Message.find(params[:id])
+
+    case @message.target_type
+    when 'Reply'
+      @reply = Reply.find(@message.target_id)
+      @post = @reply.post
+      redirect_to "#{frontend_post_path(@post)}#reply#{@reply.id}"
+    else
+
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    if @message.persisted?
+      flash[:error] = '因为以下原因，删除消息失败。'
+      render :back
+    else
+      flash[:notice] = '你已经成功删除了该消息。'
+      redirect_to admin_messages_path
+    end
+  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,7 @@ class Reply < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  has_one :blogger, through: :post, source: 'user'
   has_many :messages, as: :target, dependent: :destroy
 
   validates :user_id, presence: true
@@ -9,9 +10,22 @@ class Reply < ActiveRecord::Base
   validates :content, presence: true, allow_blank: false
 
   before_save :validate_sensitive
+  after_create :message_to_blogger
 
   def published_time
     self.created_at.strftime('%Y-%m-%d %H:%M')
+  end
+
+  private
+  def message_to_blogger
+    Message.create(
+      is_read: false,
+      target_id: self.id,
+      target_type: self.class.to_s,
+      user_id: self.blogger.id,
+      from_user_id: self.user.id,
+      body: "#{self.user.human_name} 评论了你的文章 #{self.post.title}"
+    )
   end
 
   def validate_sensitive
