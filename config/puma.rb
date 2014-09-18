1af1e67ffa6d63e7742ea0da3b0b46d4684df10e
Teddy <agilejzl@gmail.com>
2014-09-18 13:22:14 +0800

    add puma for server

---
+++ config/puma.rb
@@ -0,0 +1,13 @@
+
+pidfile "/tmp/blog-eye.pid"
+state_path "/tmp/blog-eye.state"
+daemonize true
+workers 0
+threads 0,32
+bind 'tcp://0.0.0.0:8090'
+environment 'production'
+on_restart do
+  puts 'Blog Eye on restart...'
+end
+
+preload_app!
