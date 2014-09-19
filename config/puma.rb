03088e9a219bde8b2eccc5baa4d25df59ea6433d
agilejzl <agilejzl@gmail.com>
2014-09-19 22:54:51 +0800

    fix javascript load error

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ pidfile "/tmp/blog-eye.pid"
 state_path "/tmp/blog-eye.state"
 daemonize true
 workers 0
-threads 0,32
+threads 0,16
 bind 'tcp://0.0.0.0:8090'
 environment 'production'
 on_restart do
